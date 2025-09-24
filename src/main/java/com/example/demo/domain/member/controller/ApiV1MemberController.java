229c01c0c05526469718af469eaedab6c86757ef
ZarKljh <zarkljh@gmail.com>
2025-09-24 16:47:29 +0900

    cookie confirm

--- src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
@@ -7,14 +7,16 @@ import com.example.demo.domain.member.service.MemberService;
 import com.example.demo.global.jwt.JwtProvider;
 import com.example.demo.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Map;
+
+@RestController
 @RequestMapping(value="/api/v1/members")
 @RequiredArgsConstructor
 @Tag(name="ApiV1MemberController", description ="회원 인증/인가 API")
@@ -36,12 +38,35 @@ public class ApiV1MemberController {
     }
 
     @PostMapping("/login")
-    public RsData<?> login(@Valid @RequestBody MemberRequest memberRequest){
+    public RsData<MemberResponse> login(@Valid @RequestBody MemberRequest memberRequest, HttpServletResponse res){
         Member member = memberService.getMember(memberRequest.getUsername());
 
         //access token 발급
-        String token = jwtProvider.genAccessToken(member);
+        String accessToken = jwtProvider.genAccessToken(member);
+        /*자바에서 클라이언트에게 전달할 때 사용하는 개체*/
+        /*postman에서 cookie탭에서 결과를 확인가능하다*/
+        res.addCookie(new Cookie("accessToken", accessToken));
 
-        return RsData.of("200", "토큰 발급 성공", token);
+        return RsData.of("200", "토큰 발급 성공 : " + accessToken, new MemberResponse(member));
+    }
+
+    /*쿠키를 토큰으로 꺼내오는 메소드*/
+    @GetMapping("/me")
+    public RsData<MemberResponse> me(HttpServletRequest req){
+        Cookie[] cookies = req.getCookies();
+        String accessToken = "";
+
+        for(Cookie cookie : cookies){
+            if("accessToken".equals(cookie.getName())){
+                accessToken = cookie.getValue();
+            }
+
+        }
+        /*토큰안에 있는 사용자 정보를 꺼내온다*/
+        Map<String, Object> claims = jwtProvider.getClaims(accessToken);
+        String username = (String)claims.get("username");
+        Member member = this.memberService.getMember(username);
+
+        return RsData.of("200", "내 회원 정보", new MemberResponse(member));
     }
 }
